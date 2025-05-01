e58033b6f1e6f5ab0cea0b182553eb3fd5271f7d
jainAmishhh <jainamish1511@gmail.com>
2025-05-02 01:12:15 +0530

    edit the fies

---
+++ 01basicreact/src/Hello.jsx
@@ -0,0 +1,7 @@
+function Hello() {
+    return(
+        <h1>Hello there! I'm using CRA</h1>
+    )
+}
+
+export default Hello;
---
+++ 01basicvite/src/Hello.jsx
@@ -0,0 +1,7 @@
+function Hello() {
+    return (
+        <h1>Hello there! I'm learning REACT</h1>
+    )
+}
+
+export default Hello;
