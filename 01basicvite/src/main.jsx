c5f6b9849e764180441d2b9a452eb080ee7e8572
jainAmishhh <jainamish1511@gmail.com>
2025-05-02 16:42:50 +0530

    created a new folder named as customReact and also add custom react to the main.jsx in the 01basicvite/src/ folder

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -2,9 +2,39 @@
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
+import React from 'react';
+
+function MyApp() {
+    return (
+        <div>
+            <h1>Hyy! This is custom app</h1>            
+        </div>
+    )
+}
+
+const reactElement = {
+    type: "a",
+    props: {
+        href: "https://www.google.com",
+        target: "_blank"
+    },
+    display: "Click me to visit google.com"
+};
+
+const NewReactElement = React.createElement(
+    "a",
+    { 
+        href: "https://www.google.com",
+        target: "_blank"
+    },
+    "Click me to visit google"
+);
 
 createRoot(document.getElementById('root')).render(
 
-    <App />
+    // <App />
+    // <MyApp />
+    // reactElement
+    NewReactElement
 
 )
